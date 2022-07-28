21f18c64c118c8f78ab60a2110daa3ffea75cce9
VikasKurella <vikaschandrakurella@gmail.com>
2022-07-27 20:25:52 -0500

    remove unnecessary onClick in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Image from '../images/troll-face.png'
 const Header = () => {
     return (
         <header className='header'>
-            <img src={Image} className='header-image' onClick='./' alt='troll-face'/>
+            <img src={Image} className='header-image' alt='troll-face'/>
             <h2 className='header-title'>Meme Generator</h2>
         </header>
     )
